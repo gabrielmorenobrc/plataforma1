cd7283e85db9b0206f35a6a943dcd2cf086cea87
lgmaestropiedra <guillermo-maestropiedra@hotmail.com>
2023-09-28 15:48:13 -0300

    fixes

--- src/plataforma1/wicket/semantic/SemanticResourceReference.java
+++ src/plataforma1/wicket/semantic/SemanticResourceReference.java
@@ -1,10 +1,10 @@
 package plataforma1.wicket.semantic;
 
-import com.vaynberg.wicket.select2.ApplicationSettings;
 import org.apache.wicket.markup.head.*;
 import org.apache.wicket.request.resource.CssResourceReference;
 import org.apache.wicket.request.resource.JavaScriptResourceReference;
 import org.apache.wicket.resource.JQueryResourceReference;
+import org.wicketstuff.select2.ApplicationSettings;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,7 +26,7 @@ public class SemanticResourceReference extends JavaScriptResourceReference {
         JavaScriptReferenceHeaderItem jquery = JavaScriptHeaderItem.forReference(JQueryResourceReference.getV3());
         CssReferenceHeaderItem style = CssHeaderItem.forReference(new CssResourceReference(SemanticResourceReference.class, "resources/semantic.min.css"));
         CssReferenceHeaderItem semanticOverrides = CssHeaderItem.forReference(new CssResourceReference(SemanticResourceReference.class, "resources/semantic-overrides.css"));
-        CssReferenceHeaderItem select2Style = CssHeaderItem.forReference(new CssResourceReference(ApplicationSettings.class, "res/select2.css"));
+        CssReferenceHeaderItem select2Style = CssHeaderItem.forReference(new CssResourceReference(ApplicationSettings.class, "res/css/select2.css"));
         CssReferenceHeaderItem select2Overrides = CssHeaderItem.forReference(new CssResourceReference(SemanticResourceReference.class, "resources/select2-overrides.css"));
         return Arrays.asList(jquery, style, semanticOverrides, select2Style, select2Overrides);
     }
